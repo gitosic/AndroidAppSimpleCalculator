c849f0b11709fc42b2399919217609e68cde6be6
vitaliy <vitaliu@yandex.ru>
2023-08-09 14:18:02 +0300

    Initial commit

---
+++ app/src/main/java/com/example/simplecalculator/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.simplecalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView resultTextView;
+    private EditText number_field_1, number_field_2;
+    private Button add_button;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        resultTextView = findViewById(R.id.resultTextView);
+        number_field_1 = findViewById(R.id.number_field_1);
+        number_field_2 = findViewById(R.id.number_field_2);
+        add_button = findViewById(R.id.add_button);
+
+        add_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                float num1 = Float.parseFloat(number_field_1.getText().toString());
+                float num2 = Float.parseFloat(number_field_2.getText().toString());
+                float res = num1 + num2;
+                resultTextView.setText(String.valueOf(res));
+            }
+        });
+    }
+
+}
